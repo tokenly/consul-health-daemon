d066e16fc3ecc92bf85bef1f93abb493235de2e3
Devon Weller <devon@tokenly.co>
2016-05-07 12:06:25 -0500

    Use guzzle 5 or 6.  Switch to PSR-4 autoloading.

--- src/ConsulClient.php
+++ src/ConsulClient.php
@@ -14,9 +14,12 @@ use Illuminate\Support\Facades\Log;
 class ConsulClient
 {
 
+    protected $consul_url = null;
+
     public function __construct($consul_url=null) {
         if ($consul_url === null) { $consul_url = \Illuminate\Support\Facades\Config::get('consul-health.consul_url'); }
-        $this->guzzle_client = new Client(['base_uri' => $consul_url]);
+        $this->consul_url = rtrim($consul_url, '/');
+        $this->guzzle_client = new Client();
     }
 
     public function healthUp($container_name) {
@@ -28,7 +31,7 @@ class ConsulClient
 
     public function checkPass($check_id) {
         try {
-            $this->guzzle_client->get('/v1/agent/check/pass/'.urlencode($check_id));
+            $this->guzzle_client->get($this->consul_url.'/v1/agent/check/pass/'.urlencode($check_id));
             return true;
         } catch (Exception $e) {
             Log::warning("failed to update check pass: ".$check_id);
@@ -37,7 +40,7 @@ class ConsulClient
     }
     public function checkWarn($check_id, $note=null) {
         try {
-            $this->guzzle_client->get('/v1/agent/check/warn/'.urlencode($check_id), ['query' => ['note' => $note]]);
+            $this->guzzle_client->get($this->consul_url.'/v1/agent/check/warn/'.urlencode($check_id), ['query' => ['note' => $note]]);
             return true;
         } catch (Exception $e) {
             Log::warning("failed to update check warn: ".$check_id);
@@ -46,7 +49,7 @@ class ConsulClient
     }
     public function checkFail($check_id, $note=null) {
         try {
-            $this->guzzle_client->get('/v1/agent/check/fail/'.urlencode($check_id), ['query' => ['note' => $note]]);
+            $this->guzzle_client->get($this->consul_url.'/v1/agent/check/fail/'.urlencode($check_id), ['query' => ['note' => $note]]);
             return true;
         } catch (Exception $e) {
             Log::warning("failed to update check failure: ".$check_id);
@@ -55,16 +58,16 @@ class ConsulClient
     }
 
     public function setKeyValue($key, $value) {
-        $this->guzzle_client->put('/v1/kv/'.urlencode($key), ['body' => (string)$value]);
+        $this->guzzle_client->put($this->consul_url.'/v1/kv/'.urlencode($key), ['body' => (string)$value]);
     }
 
     public function deleteKey($key) {
-        $this->guzzle_client->delete('/v1/kv/'.urlencode($key));
+        $this->guzzle_client->delete($this->consul_url.'/v1/kv/'.urlencode($key));
     }
 
     public function getKeyValue($key) {
         try {
-            $response = $this->guzzle_client->get('/v1/kv/'.urlencode($key));
+            $response = $this->guzzle_client->get($this->consul_url.'/v1/kv/'.urlencode($key));
         } catch (ClientException $e) {
             $e_respone = $e->getResponse();
             if ($e_respone->getStatusCode() == 404) {
